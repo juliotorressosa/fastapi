8284c968f95273e21213840ab865188a37128f06
juliotorressosa <juliotorressosa@gmail.com>
2026-03-10 17:48:34 -0600

    Update auth.py

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -27,6 +28,14 @@ def get_db():
 
 db_dependency = Annotated[Session,Depends(get_db)]
 
+def authenticate_user(username:str,password:str,db):
+    user = db.query(Users).filter(Users.username==username).first()
+    if not user:
+        return False
+    if not bcrypt_contex.verify(password,user.hashed):
+        return False
+    return True
+
 @router.post("/auth",status_code=status.HTTP_201_CREATED)
 async def create_user(db:db_dependency,
                       create_user_request: CreateUserRequest):
@@ -42,3 +51,13 @@ async def create_user(db:db_dependency,
     
     db.add(create_user_model)
     db.commit
+
+
+@router.post("/token")
+async def login_for_access_token(form_data:Annotated[OAuth2PasswordRequestForm,Depends()],
+                                 db:db_dependency):   
+    user = authenticate_user(form_data.username, form_data.password,db)
+    if not user:
+        return 'Failed Authentication'
+    return 'Successful Authentication'
+    
