2ed29d7de42159cc61a7a89404047cd423eff364
juliotorressosa <juliotorressosa@gmail.com>
2026-03-12 13:27:39 -0600

    Update auth.py

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -72,7 +72,7 @@ async def get_current_user(token:Annotated[str,Depends(oauth2_bearer)]):
                             detail = 'Could not validate user')
              
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db:db_dependency,
                       create_user_request: CreateUserRequest):
     create_user_model = Users(
