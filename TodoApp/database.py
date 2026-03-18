3003c22b7769f661edc1bd916a1e087a2cafc0e9
juliotorressosa <juliotorressosa@gmail.com>
2026-03-18 14:59:12 -0600

    Update database.py

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -6,10 +6,21 @@ import os
 
 load_dotenv()
 
+######################################### For using sqlite3 ##########################################
+######################################### Install dependencies #######################################
+#SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'   #os.getenv('DATABASE_URL')  for using the dotenv connection method
+#engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {'check_same_thread':False})
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'   #os.getenv('DATABASE_URL')
+######################################### For using PostgreSQL #######################################
+######################################### Install dependencies #######################################
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:admin1234@localhost/TodoApplicationDatabase'   
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {'check_same_thread':False})
+######################################### For using MySQL ############################################
+######################################### Install dependencies #######################################
+#SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:admin1234@127.0.0.1:3306/TodoApplicationDatabase'   
+#engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
