ae45dc20ed0ed942fca7295a19cbc7b7ec9e2bf9
juliotorressosa <juliotorressosa@gmail.com>
2026-03-18 14:59:15 -0600

    Update admin.py

--- TodoApp/routers/admin.py
+++ TodoApp/routers/admin.py
@@ -40,6 +40,6 @@ async def delete_todo(user:user_dependency,db:db_dependency,todo_id:int=Path(gt=
     todo_model = db.query(Todos).filter(Todos.id==todo_id).first()
     if todo_model is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    db.query(Todos).fiter(Todos.id==todo_id).delete() 
+    db.query(Todos).filter(Todos.id==todo_id).delete() 
     db.commit()
     
