00f6db456d13a3cabd26f3e125270358c0672d5c
juliotorressosa <juliotorressosa@gmail.com>
2026-03-18 14:59:18 -0600

    Update auth.py

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -64,7 +64,7 @@ async def get_current_user(token:Annotated[str,Depends(oauth2_bearer)]):
         username:str= payload.get('sub')
         user_id: int =payload.get('id')
         user_role:int=payload.get('role')
-        if username in None or user_id is None:
+        if username is None or user_id is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                 detail = 'Could not validate user.')
         return {'username':username, 'id':user_id,'user_role':user_role}
@@ -86,7 +86,7 @@ async def create_user(db:db_dependency,
         role = create_user_request.role
     )
     db.add(create_user_model)
-    db.commit
+    db.commit()
 
 
 @router.post("/token",response_model = Token)
